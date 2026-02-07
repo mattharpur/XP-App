bf9265408da671d4d993f6f426924bb3d1018847
Matt Harpur <mattharpur@outlook.com>
2026-02-06 21:08:51 -0500

    added tab bar
    added tab bar

--- XP-App/ContentView.swift
+++ XP-App/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+            
         }
-        .padding()
+    }
+    
+    
+}
+
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+            }
+            .navigationTitle(Text("Settings"))
+        }
+        
+        
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+            }
+            .navigationTitle(Text("Home"))
+        }
+        
+        
     }
 }
 
